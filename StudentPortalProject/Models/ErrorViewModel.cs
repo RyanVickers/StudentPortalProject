cc21c871ee8324ad41a7b15058a8f9712b64a03c
Ryan Vickers <rdvicker@lakeheadu.ca>
2023-01-12 15:03:07 -0500

    Add project files.

---
+++ StudentPortalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentPortalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
