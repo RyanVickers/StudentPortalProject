3e9c261e0e980bf1d0331aef8f489d157b87c519
NikBrennan <ndbrenna@lakeheadu.ca>
2023-02-23 15:15:37 -0500

    Fixed return after group creation.

--- StudentPortalProject/Controllers/GroupsController.cs
+++ StudentPortalProject/Controllers/GroupsController.cs
@@ -62,11 +62,11 @@ namespace StudentPortalProject.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,GroupName,CourseId")] Group @group)
         {
-            //var course = await _context.Course.FindAsync(group.CourseId);
-            //group.Course = course;
+            var course = await _context.Course.FindAsync(group.CourseId);
+            group.Course = course;
             _context.Add(group);
             await _context.SaveChangesAsync();
-            return View(@group);
+            return RedirectToAction("GroupList", "Courses", new { id = group.CourseId });
         }
 
         // GET: Groups/Edit/5
